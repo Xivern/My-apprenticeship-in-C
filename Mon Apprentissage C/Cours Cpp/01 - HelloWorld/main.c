c76894e56db8fdd2244371668c33b84f97c782f7
Guillaume <guillaume77410@live.fr>
2024-09-16 14:50:12 +0200

    Ajout du programme HelloWorld

---
+++ Mon Apprentissage C/Cours Cpp/01 - HelloWorld/main.c
@@ -0,0 +1,12 @@
+//Directive de préprocesseur permettant de charger des bibliothèques pour notre programme
+#include <stdio.h>
+#include <stdlib.h>
+
+//Fonction principale de notre projet, celle-ci est exécutée lorsque nous lançons notre programme.
+int main(int argc, char *argv[])
+{
+    printf("Hello world !\n");          //Permet d'afficher le texte "Hello world!" sur notre console.
+                                        //Backslash n permet d'effectuer un retour à la ligne.
+                                        //Backslash t permet quant à lui d'effectuer une tabulation.
+    return 0;
+}
